65f9e9af95a41a008ce0131d6f849f46eaf2566c
K1ngadr1an <adricl2015@gmail.com>
2024-07-29 17:11:37 +0200

    feat new js file and applied new changes

---
+++ game.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById("canvas");
+const canvasContext = canvas.getContext("2d");
+
+const pacmanFrames = document.getElementById("animations");
+const ghostFrames = document.getElementById("ghost");
